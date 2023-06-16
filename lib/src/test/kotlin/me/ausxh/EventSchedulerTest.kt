7cc6052cada782afce52b6861ec5b249e831c1a0
Harjot Singh <harjot.singh0712@gmail.com>
2023-06-15 20:51:36 -0700

    Set up gradle library project

---
+++ lib/src/test/kotlin/me/ausxh/EventSchedulerTest.kt
@@ -0,0 +1,13 @@
+package me.ausxh.scheduleGen
+
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class LibraryTest {
+    @Test fun eventCombinations() {
+
+    }
+}
