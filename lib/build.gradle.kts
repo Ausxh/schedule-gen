0cbc180c8699ca2cb0dc299cc5b6218bec2d4b06
Harjot Singh <harjot.singh0712@gmail.com>
2023-06-15 22:15:24 -0700

    Downgrade kotlin plugin version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ group = "me.ausxh.scheduleGen"
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
